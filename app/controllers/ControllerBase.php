044667d8a58dd2f72ea02b4c2c4c170d43ffdd3b
Nikos Dimopoulos <nikos@niden.net>
2012-06-22 20:34:06 -0400

    Changed the default title message for all pages

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * ControllerBase.php
+ * ControllerBase
+ *
+ * The base controller and its actions
+ *
+ * @author      Nikos Dimopoulos <nikos@niden.net>
+ * @since       2012-06-22
+ * @category    Controllers
+ * @license     MIT - https://github.com/niden/phalcon-angular-harryhogfootball/blob/master/LICENSE
+ *
+ */
 
 class ControllerBase extends Phalcon_Controller
 {
+    /**
+     * Initializes the controller
+     */
     public function initialize()
     {
-        Phalcon_Tag::prependTitle('INVO | ');
+        Phalcon_Tag::prependTitle('HHF G&KB Awards | ');
     }
 }
