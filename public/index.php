c464da1a8728347d3b938c132ad99ad0183529b0
Nikos Dimopoulos <nikos@niden.net>
2012-06-24 11:45:06 -0400

    Changed the init using the autoloader. Stored config in registry to be used later on if needed.

--- public/index.php
+++ public/index.php
@@ -4,17 +4,34 @@ error_reporting(E_ALL);
 
 try {
 
-    require __DIR__ . '/../app/controllers/ControllerBase.php';
-    require __DIR__ . '/../app/library/Elements.php';
-    require __DIR__ . '/../app/library/HHF/Breadcrumbs.php';
+    $app     = __DIR__ . '/../app/';
+    $library = $app . 'library/';
 
+    // Creates the autoloader
+    $loader = new Phalcon_Loader();
+
+    // Register some classes
+    $loader->registerClasses(
+        array(
+            'ControllerBase'    => $app     . 'controllers/ControllerBase.php',
+            'Elements'          => $library . 'Elements.php',
+            'niden_Registry'    => $library . 'niden/Registry.php',
+            'niden_Breadcrumbs' => $library . 'niden/Breadcrumbs.php',
+        )
+    );
+
+    //register autoloader
+    $loader->register();
+
+    // Get the config
+    $config = new Phalcon_Config_Adapter_Ini($app . 'config/config.ini');
+
+    niden_Registry::set('config', $config);
+
+    // Start the session
     Phalcon_Session::start();
 
     $front = Phalcon_Controller_Front::getInstance();
-
-    $config = new Phalcon_Config_Adapter_Ini(
-        __DIR__ . '/../app/config/config.ini'
-    );
     $front->setConfig($config);
 
     echo $front->dispatchLoop()->getContent();
