59c9bcc0ed45a6267e2583b6935543284907046a
Nikos Dimopoulos <nikos@niden.net>
2012-06-24 22:16:25 -0400

    Changes to the model for scores

---
+++ app/migrations/1.0.2/contact.php
@@ -0,0 +1,59 @@
+<?php 
+
+use Phalcon_Db_Column as Column;
+use Phalcon_Db_Index as Index;
+use Phalcon_Db_Reference as Reference;
+
+class ContactMigration_102 extends Phalcon_Model_Migration {
+
+	public function up(){
+		$this->morphTable('contact', array(
+			'columns' => array(
+				new Column('id', array(
+					'type' => Column::TYPE_INTEGER,
+					'size' => 10,
+					'unsigned' => true,
+					'primary' => true,
+					'notNull' => true,
+					'autoIncrement' => true,
+					'first' => true
+				)),
+				new Column('name', array(
+					'type' => Column::TYPE_VARCHAR,
+					'size' => 70,
+					'notNull' => true,
+					'after' => 'id'
+				)),
+				new Column('email', array(
+					'type' => Column::TYPE_VARCHAR,
+					'size' => 70,
+					'notNull' => true,
+					'after' => 'name'
+				)),
+				new Column('comments', array(
+					'type' => Column::TYPE_TEXT,
+					'notNull' => true,
+					'after' => 'email'
+				)),
+				new Column('createdAt', array(
+					'type' => Column::TYPE_INTEGER,
+					'size' => 11,
+					'notNull' => true,
+					'after' => 'comments'
+				))
+			),
+			'indexes' => array(
+				new Index('PRIMARY', array(
+					'id'
+				))
+			),
+			'options' => array(
+				'TABLE_TYPE' => 'BASE TABLE',
+				'AUTO_INCREMENT' => '1',
+				'ENGINE' => 'InnoDB',
+				'TABLE_COLLATION' => 'utf8_unicode_ci'
+			)
+		));
+	}
+
+}
---
+++ app/migrations/1.0.2/episodes.php
@@ -0,0 +1,54 @@
+<?php 
+
+use Phalcon_Db_Column as Column;
+use Phalcon_Db_Index as Index;
+use Phalcon_Db_Reference as Reference;
+
+class EpisodesMigration_102 extends Phalcon_Model_Migration {
+
+	public function up(){
+		$this->morphTable('episodes', array(
+			'columns' => array(
+				new Column('id', array(
+					'type' => Column::TYPE_INTEGER,
+					'size' => 10,
+					'unsigned' => true,
+					'primary' => true,
+					'notNull' => true,
+					'autoIncrement' => true,
+					'first' => true
+				)),
+				new Column('number', array(
+					'type' => Column::TYPE_VARCHAR,
+					'size' => 5,
+					'notNull' => true,
+					'after' => 'id'
+				)),
+				new Column('summary', array(
+					'type' => Column::TYPE_TEXT,
+					'notNull' => true,
+					'after' => 'number'
+				)),
+				new Column('airDate', array(
+					'type' => Column::TYPE_INTEGER,
+					'size' => 10,
+					'unsigned' => true,
+					'notNull' => true,
+					'after' => 'summary'
+				))
+			),
+			'indexes' => array(
+				new Index('PRIMARY', array(
+					'id'
+				))
+			),
+			'options' => array(
+				'TABLE_TYPE' => 'BASE TABLE',
+				'AUTO_INCREMENT' => '1',
+				'ENGINE' => 'InnoDB',
+				'TABLE_COLLATION' => 'utf8_unicode_ci'
+			)
+		));
+	}
+
+}
